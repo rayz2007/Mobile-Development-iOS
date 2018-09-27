fee2b93b6dbff982e2aba45c9ecc2c6c7304fe75
rayz2007 <rayz2007@uw.edu>
2018-09-26 18:52:14 -0700

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ray Zhang on 9/26/18.
+//  Copyright © 2018 Ray Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
